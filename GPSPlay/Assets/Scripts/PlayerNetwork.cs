ce9f098e8494157d239d5f5839d67546cfd19048
KillerDoll12 <53497444+EricChelmu@users.noreply.github.com>
2023-03-21 18:39:04 +0100

    network variables added
    variables that synchronize between clients, with custom variables included

--- GPSPlay/Assets/Scripts/PlayerNetwork.cs
+++ GPSPlay/Assets/Scripts/PlayerNetwork.cs
@@ -5,9 +5,43 @@ using Unity.Netcode;
 
 public class PlayerNetwork : NetworkBehaviour
 {
+    private NetworkVariable<MyCustomData> randomNumber = new NetworkVariable<MyCustomData>(new MyCustomData
+    {
+        _int = 56,
+        _bool = false,
+    }, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+    public struct MyCustomData : INetworkSerializable
+    {
+        public int _int;
+        public bool _bool;
+        public string _message;
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref _int);
+            serializer.SerializeValue(ref _bool);
+        }
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        randomNumber.OnValueChanged += (MyCustomData previousValue, MyCustomData newValue) =>
+        {
+            Debug.Log(OwnerClientId + "; " + newValue._int + "; " + newValue._bool);
+        };
+    }
     private void Update()
     {
         if (!IsOwner) return;
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            randomNumber.Value = new MyCustomData
+            {
+                _int = 10,
+                _bool = false,
+            };
+        }
 
         Vector3 moveDir = new Vector3(0, 0, 0);
 
