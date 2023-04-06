758f4f0184e0ddd8fa90a66197f1bce921173c7b
Eric Chelmu <eric.chelmu12@gmail.com>
2023-04-06 11:26:18 +0200

    map instantiation

--- GPSPlay/Assets/Scripts/Multiplayer/LocationFixed.cs
+++ GPSPlay/Assets/Scripts/Multiplayer/LocationFixed.cs
@@ -8,11 +8,14 @@ namespace Multiplayer
 {
     public class LocationFixed : MonoBehaviour
     {
+        [SerializeField]
+        private GameObject mapPrefab;
         private LocationProviderFactory locationProviderFactory;
         private AbstractMap map;
 
         private void Awake()
         {
+            Instantiate(mapPrefab);
             map = GameObject.FindGameObjectWithTag("map").GetComponent<AbstractMap>();
             locationProviderFactory = GetComponent<LocationProviderFactory>();
             locationProviderFactory.mapManager = map;
