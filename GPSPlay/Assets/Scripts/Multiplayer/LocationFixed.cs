86c01de3dc4f886ff278d5a7ae524844b230cb38
Eric Chelmu <eric.chelmu12@gmail.com>
2023-04-06 11:37:49 +0200

    map fix

--- GPSPlay/Assets/Scripts/Multiplayer/LocationFixed.cs
+++ GPSPlay/Assets/Scripts/Multiplayer/LocationFixed.cs
@@ -8,14 +8,11 @@ namespace Multiplayer
 {
     public class LocationFixed : MonoBehaviour
     {
-        [SerializeField]
-        private GameObject mapPrefab;
         private LocationProviderFactory locationProviderFactory;
         private AbstractMap map;
 
         private void Awake()
         {
-            Instantiate(mapPrefab);
             map = GameObject.FindGameObjectWithTag("map").GetComponent<AbstractMap>();
             locationProviderFactory = GetComponent<LocationProviderFactory>();
             locationProviderFactory.mapManager = map;
---
+++ GPSPlay/Assets/Scripts/SpawnMap.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnMap : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mapPrefab;
+
+    private void Awake()
+    {
+        Instantiate(mapPrefab);
+    }
+}
