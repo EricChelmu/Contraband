5d858f3e8b8c38c9c982d90f3de51a9fda5814db
Miles-rgb <milan1466.kokoska@gmail.com>
2023-05-25 01:48:31 +0200

    retry

--- GPSPlay/Assets/Scripts/Map/POI_Interact.cs
+++ GPSPlay/Assets/Scripts/Map/POI_Interact.cs
@@ -3,18 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class POI_Interact : MonoBehaviour
-{
-    public Text message;
-    public string n_message;
+namespace Map
+{    public class POI_Interact : MonoBehaviour
+    {
+        private QuestManager _questManager;
+        public Text message;        
+        private string my_message;
 
-    private void Start()
-    {
-        message = GameObject.FindGameObjectWithTag("ChatLog").GetComponent<Text>();
-    }
-    private void OnMouseDown()
-    {
-        Debug.Log("Clicked");
-        message.text = "Hello World!";
+        private void Start()
+        {
+            message = GameObject.FindGameObjectWithTag("ChatLog").GetComponent<Text>();
+            _questManager = GameObject.FindGameObjectWithTag("Manager").GetComponent<QuestManager>();
+            my_message = _questManager.n_message[0];
+            _questManager.n_message.Remove(my_message);
+        }
+        private void OnMouseDown()
+        {
+            message.text = my_message;
+        }
     }
 }
+
