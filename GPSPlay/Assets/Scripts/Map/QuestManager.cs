92f5c16519abb40445aaee16746e19ce27954c35
Miles-rgb <milan1466.kokoska@gmail.com>
2023-05-24 23:55:48 +0200

    FollowPlayer

---
+++ GPSPlay/Assets/Scripts/Map/QuestManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Map
+{    public class QuestManager : MonoBehaviour
+    {
+        public List<string> n_message = new List<string>();
+    }
+
+}
